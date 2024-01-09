82b109909bbfd87d8aa45575efdf592b537ad727
Lion1141 <vodopyanov.le@yandex.ru>
2024-01-10 04:22:13 +1000

    fix: Выполнено ФЗ-9, обновлено для git

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.*;
-import java.time.Duration;
 import java.time.LocalDate;
 
 /**
@@ -16,7 +15,7 @@ public class Film {
         Integer id;
     @NotBlank(message = "Название фильма не должно быть пустым")
     String name;
-    @Size(max=200)
+    @Size(max = 200)
     String description;
     LocalDate releaseDate; //не смог подобрать нужную аннотацию
     @Positive
--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,7 +18,7 @@ public class FilmControllerTest extends FilmController {
                 .name("Inception")
                 .description("A heist movie that takes place within the architecture of the mind")
                 .releaseDate(LocalDate.of(2010, 7, 16))
-                .duration(Duration.ofMinutes(148))
+                .duration(148L)
                 .build();
         Film addedFilm = filmController.addFilm(film);
         assertEquals(film, addedFilm);
@@ -34,7 +32,7 @@ public class FilmControllerTest extends FilmController {
                 .name("Inception")
                 .description("A heist movie that takes place within the architecture of the mind")
                 .releaseDate(LocalDate.of(2010, 7, 16))
-                .duration(Duration.ofMinutes(148))
+                .duration(148L)
                 .build();
         films.put(1, existingFilm);
 
@@ -43,7 +41,7 @@ public class FilmControllerTest extends FilmController {
                 .name("Inception: Special Edition")
                 .description("A heist movie that takes place within the architecture of the mind, now with extra scenes")
                 .releaseDate(LocalDate.of(2010, 7, 16))
-                .duration(Duration.ofMinutes(180))
+                .duration(148L)
                 .build();
         Film newFilm = filmController.updateFilm(updatedFilm);
         assertEquals(updatedFilm, newFilm);

230d417ad43e15ccda49b87dca227e9aa4c357f6
Lion1141 <140077613+Lion1141@users.noreply.github.com>
2024-01-10 04:05:11 +1000

    Revert "init: Выполнено ФЗ-9"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,26 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
-import lombok.Data;
-
-import javax.validation.constraints.*;
-import java.time.Duration;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Film.
  */
-@Data
-@Builder
+@Getter
+@Setter
 public class Film {
-        Integer id;
-    @NotBlank(message = "Название фильма не должно быть пустым")
-    String name;
-    @Size(max=200)
-    String description;
-    LocalDate releaseDate; //не смог подобрать нужную аннотацию
-    @Positive
-    Long duration;
-
-
 }

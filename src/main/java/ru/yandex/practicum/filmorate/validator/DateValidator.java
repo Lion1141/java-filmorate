dfc12ab1c6114223c3df07a26e890dc7aeaf944a
Lion1141 <vodopyanov.le@yandex.ru>
2024-01-11 00:43:46 +1000

    fix: Исправлены замечания, реализована проверка валидации при помощи аннотаций

--- src/main/java/ru/yandex/practicum/filmorate/validator/DateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/DateValidator.java
@@ -6,13 +6,16 @@ import java.time.LocalDate;
 
 public class DateValidator implements ConstraintValidator<IsAfter, LocalDate> {
     String validDate;
+
     @Override
     public void initialize(IsAfter constraintAnnotation) {
         validDate = constraintAnnotation.current();
     }
+
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
         String[] splitDate = validDate.split("-");
-        return date.isAfter(LocalDate.of(Integer.valueOf(splitDate[0]), Integer.valueOf(splitDate[1]), Integer.valueOf(splitDate[2])));
+        return date.isAfter(LocalDate.of(Integer.valueOf(splitDate[0]), Integer.valueOf(splitDate[1]),
+                Integer.valueOf(splitDate[2])));
     }
 }

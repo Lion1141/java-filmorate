e3dd5d981d7e0f7100b61e72875e9f513073e9fa
Lion1141 <vodopyanov.le@yandex.ru>
2024-02-03 21:21:34 +1000

    feat: выполнено ФЗ-10, исправлены замечания по кодстайлу

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.validator.IsAfter;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 /**

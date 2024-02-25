61fa094b6f94a0a8d0afa5ac24f16a6b12e523ff
Лев <vodopyanov.le@yandex.ru>
2024-02-25 22:14:11 +1000

    feat: Выполнено ФЗ-11, исправлены замечания по кодстайлу

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storages.FilmStorage;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Service

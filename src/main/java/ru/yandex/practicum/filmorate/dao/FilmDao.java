6a33bd615412a9eac8cdb9a45e91bb8ec0cf2974
Лев <vodopyanov.le@yandex.ru>
2024-02-27 19:42:40 +1000

    fix: Исправлены замечания, выполнен рефакторинг в соответсвии с рекомендациями

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDao.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class FilmDao {
+
+    public Integer id;
+
+    public String name;
+
+    public String description;
+
+    public LocalDate releaseDate;
+
+    public Long duration;
+
+    public int mpa_id;
+
+}
